f776f234466510b593d86dc5b4176ac6f1f51b01
Michael Redig <mredig@gmail.com>
2024-02-16 00:51:21 -0600

    (feat) add character scanning and peeking

--- Sources/DataScanner/DataScanner.swift
+++ Sources/DataScanner/DataScanner.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftPizzaSnips
 
 public struct DataScanner {
 	public let data: Data
@@ -61,18 +62,43 @@ public struct DataScanner {
 		}
 	}
 
+	public mutating func scanUTF8Character() throws -> Character {
+		let byte = try advanceByte()
+
+		let scalar = UnicodeScalar(byte)
+		let char = Character(scalar)
+		guard char.utf8.isOccupied else { throw Error.invalidCharacter }
+		return char
+	}
+
 	/// Outputs the current byte, then advances by one.
+	@discardableResult
 	public mutating func advanceByte() throws -> UInt8 {
-		guard isAtEnd == false else { throw Error.isAtEnd }
-		let byte = data[currentOffset]
+		let byte = try peekByte()
 
 		currentOffset += 1
 		return byte
 	}
 
+	public func peekByte() throws -> UInt8 {
+		guard isAtEnd == false else { throw Error.isAtEnd }
+		let byte = data[currentOffset]
+
+		return byte
+	}
+
 	/// Provides each byte in a Data object. If `count` is negative, bytes start at the lower index and end
 	/// at `currentIndex - 1`. `count` must keep the resulting index within the range of available data.
+	@discardableResult
 	public mutating func advanceBytes(_ count: Int) throws -> Data {
+		let endOffset = currentOffset + count
+		let bytes = try peekBytes(count)
+
+		currentOffset = endOffset
+		return bytes
+	}
+
+	public func peekBytes(_ count: Int) throws -> Data {
 		let endOffset = currentOffset + count
 		let bytes: Data
 		if count >= 0 {
@@ -83,12 +109,12 @@ public struct DataScanner {
 			bytes = Data(data[endOffset..<currentOffset].reversed())
 		}
 
-		currentOffset = endOffset
 		return bytes
 	}
 
 	public enum Error: Swift.Error {
 		case isAtEnd
 		case overflowError
+		case invalidCharacter
 	}
 }
