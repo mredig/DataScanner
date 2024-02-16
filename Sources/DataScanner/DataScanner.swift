6da369d2fa8ec39a96954daad12a7b18104f016e
Michael Redig <mredig@gmail.com>
2024-02-16 02:42:42 -0600

    (fix) character scanning works

--- Sources/DataScanner/DataScanner.swift
+++ Sources/DataScanner/DataScanner.swift
@@ -63,12 +63,54 @@ public struct DataScanner {
 	}
 
 	public mutating func scanUTF8Character() throws -> Character {
-		let byte = try advanceByte()
+		let (char, count) = try _peekUTF8Character()
 
-		let scalar = UnicodeScalar(byte)
-		let char = Character(scalar)
-		guard char.utf8.isOccupied else { throw Error.invalidCharacter }
+		currentOffset += count
 		return char
+	}
+
+	public func peekUTF8Character() throws -> Character {
+		try _peekUTF8Character().char
+	}
+
+	private func _peekUTF8Character() throws -> (char: Character, byteCount: Int) {
+		let firstByte = try peekByte()
+		guard firstByte != 0 else { throw Error.nullTerminated }
+
+		let bytes: Data
+		let analysis = CharacterAnalyst.analyzeByte(firstByte)
+		func confirmBytesAreContinuation(_ bytes: Data) -> Bool {
+			bytes.allSatisfy { CharacterAnalyst.analyzeByte($0) == .continuationByte }
+		}
+		switch analysis {
+		case .oneByte:
+			bytes = Data([firstByte])
+		case .twoByte:
+			bytes = try peekBytes(2)
+			guard
+				confirmBytesAreContinuation(bytes[1...])
+			else { throw Error.invalidCharacter}
+		case .threeByte:
+			bytes = try peekBytes(3)
+			guard
+				confirmBytesAreContinuation(bytes[1...])
+			else { throw Error.invalidCharacter}
+		case .fourByte:
+			bytes = try peekBytes(4)
+			guard
+				confirmBytesAreContinuation(bytes[1...])
+			else { throw Error.invalidCharacter}
+		case .continuationByte, .illegalByte:
+			throw Error.invalidCharacter
+		}
+
+		guard
+			let str = String(data: bytes, encoding: .utf8),
+			str.count == 1,
+			let char = str.first
+		else { throw Error.invalidCharacter }
+
+		return (char, bytes.count)
 	}
 
 	/// Outputs the current byte, then advances by one.
@@ -103,7 +145,7 @@ public struct DataScanner {
 		let bytes: Data
 		if count >= 0 {
 			guard endOffset <= data.endIndex else { throw Error.overflowError }
-			bytes = data[currentOffset..<endOffset]
+			bytes = Data(data[currentOffset..<endOffset])
 		} else {
 			guard endOffset >= data.startIndex else { throw Error.overflowError }
 			bytes = Data(data[endOffset..<currentOffset].reversed())
@@ -116,5 +158,50 @@ public struct DataScanner {
 		case isAtEnd
 		case overflowError
 		case invalidCharacter
+		case nullTerminated
+	}
+
+	package enum CharacterAnalyst {
+		case oneByte
+		case twoByte
+		case threeByte
+		case fourByte
+		case continuationByte
+		case illegalByte
+
+		static let oneByteValue: UInt8 = 0
+		static let oneByteMask: UInt8 = 0b10000000
+
+		static let twoByteValue: UInt8 = 0b11000000
+		static let twoByteMask: UInt8 = 0b11100000
+
+		static let threeByteValue: UInt8 = 0b11100000
+		static let threeByteMask: UInt8 = 0b11110000
+
+		static let fourByteValue: UInt8 = 0b11110000
+		static let fourByteMask: UInt8 = 0b11111000
+
+		static let continuationByteValue: UInt8 = 0b10000000
+		static let continuationByteMask: UInt8 = 0b11000000
+
+
+		static package func analyzeByte(_ byte: UInt8) -> CharacterAnalyst {
+			guard (byte & oneByteMask) != oneByteValue else {
+				return .oneByte
+			}
+			guard (byte & continuationByteMask) != continuationByteValue else {
+				return .continuationByte
+			}
+			guard (byte & twoByteMask) != twoByteValue else {
+				return .twoByte
+			}
+			guard (byte & threeByteMask) != threeByteValue else {
+				return .threeByte
+			}
+			guard (byte & fourByteMask) != fourByteValue else {
+				return .fourByte
+			}
+			return .illegalByte
+		}
 	}
 }
--- Tests/DataScannerTests/DataScannerTests.swift
+++ Tests/DataScannerTests/DataScannerTests.swift
@@ -72,4 +72,73 @@ final class DataScannerTests: XCTestCase {
 			XCTAssertEqual(expectedLE, num)
 		}
 	}
+
+	func testScanChars() throws {
+		let inputHex = "666F6F2062617220000000"
+		let data = try Data(hexString: inputHex)
+
+		var scanner = DataScanner(data: data)
+
+		let expected = "foo bar "
+		var currentIndex = expected.startIndex
+		while let char = try? scanner.scanUTF8Character() {
+			XCTAssertEqual(expected[currentIndex], char)
+			currentIndex = expected.index(after: currentIndex)
+		}
+	}
+
+	func testScanUnicodeChars() throws {
+		let inputHex = "f09f9fa7f09fa5b0e1baa2e284b3e28899c380"
+
+		let data = try Data(hexString: inputHex)
+
+		var scanner = DataScanner(data: data)
+
+		let expected = "🟧🥰Ảℳ∙À"
+		var currentIndex = expected.startIndex
+		while let char = try? scanner.scanUTF8Character() {
+			XCTAssertEqual(expected[currentIndex], char)
+			currentIndex = expected.index(after: currentIndex)
+		}
+	}
+
+	func testCharacterAnalyst() throws {
+		let illegalByte: UInt8 = 248
+		let oneByte: UInt8 = 0b01111111
+		let twoByte: UInt8 = 0b11011111
+		let threeByte: UInt8 = 0b11101111
+		let fourByte: UInt8 = 0b11110111
+		let contByte: UInt8 = 0b10111111
+
+		typealias Analyst = DataScanner.CharacterAnalyst
+
+		XCTAssertEqual(Analyst.analyzeByte(illegalByte), .illegalByte)
+		XCTAssertEqual(Analyst.analyzeByte(oneByte), .oneByte)
+		XCTAssertEqual(Analyst.analyzeByte(twoByte), .twoByte)
+		XCTAssertEqual(Analyst.analyzeByte(threeByte), .threeByte)
+		XCTAssertEqual(Analyst.analyzeByte(fourByte), .fourByte)
+		XCTAssertEqual(Analyst.analyzeByte(contByte), .continuationByte)
+
+		var alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
+		alphanum += alphanum.uppercased()
+
+		let alphaData = Data(alphanum.utf8)
+		alphaData.forEach {
+			XCTAssertEqual(Analyst.analyzeByte($0), .oneByte)
+		}
+
+		let fourBytes = ("🟧🥰", Analyst.fourByte)
+		let threeBytes = ("Ảℳ∙", Analyst.threeByte)
+		let twoBytes = ("À", Analyst.twoByte)
+		let tests = [fourBytes, threeBytes, twoBytes]
+
+		for (string, expected) in tests {
+			for char in string {
+				let charData = Data(char.utf8)
+				for (index, byte) in charData.enumerated() {
+					XCTAssertEqual(Analyst.analyzeByte(byte), index == 0 ? expected : .continuationByte)
+				}
+			}
+		}
+	}
 }
