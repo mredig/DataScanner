6234b14213d1bff6fcf8a341157d63993f1e8d2a
Michael Redig <mredig@gmail.com>
2024-02-18 00:08:51 -0600

    (refactor) more efficient toBytes

--- Sources/DataScanner/ToBytes.swift
+++ Sources/DataScanner/ToBytes.swift
@@ -16,14 +16,41 @@ public extension FixedWidthInteger {
 
 public extension BinaryFloatingPoint {
 	func toBytes(endianness: Endianness = .big) -> Data {
-		let buffer = UnsafeMutableBufferPointer<Self>.allocate(capacity: 1)
-		defer { buffer.deallocate() }
-		buffer[0] = self
-		let data = Data(buffer: buffer)
-		guard endianness != .systemEndianness else {
+		Data.valueTypeToBytes(self, endianness: endianness)
+	}
+}
+
+public extension Data {
+	/// This will only work on self contained value types. If it contains a reference type as a property (public or hidden) it won't work.
+	/// It might not even work on structs with multiple stored properties. It's really meant for things like Int, UInt, Float, Double, Bool,
+	/// etc. Use at your own risk
+	@inline(__always)
+	static func valueTypeToBytes<ValueType>(
+		_ value: ValueType,
+		endianness: Endianness = .big
+	) -> Data {
+		let typeSize = MemoryLayout.size(ofValue: value)
+
+		let pointer = UnsafeMutablePointer<ValueType>.allocate(capacity: 1)
+		pointer.pointee = value
+		defer { pointer.deallocate() }
+		let rawPointer = UnsafeRawPointer(pointer).bindMemory(to: UInt8.self, capacity: typeSize)
+
+		var data = Data(count: MemoryLayout.size(ofValue: value))
+		if endianness == .systemEndianness {
+			for i in 0..<typeSize {
+				let bytePointer = rawPointer.advanced(by: i)
+				data[i] = bytePointer.pointee
+			}
+			return data
+		} else {
+			for i in 0..<typeSize {
+				let advancing = typeSize - (i + 1)
+				let bytePointer = rawPointer.advanced(by: advancing)
+				data[i] = bytePointer.pointee
+			}
 			return data
 		}
-		return Data(data.reversed())
 	}
 }
 
@@ -36,3 +63,15 @@ public extension String {
 		return out
 	}
 }
+
+public extension Bool {
+	func toByte() -> Data {
+		switch self {
+		case true:
+			Data([1])
+		case false:
+			Data([0])
+		}
+	}
+}
+
