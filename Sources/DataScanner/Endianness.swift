0d6c1904316070a2a48a73b5da0a9bfcaa2f5ad5
Michael Redig <mredig@gmail.com>
2025-01-21 04:02:19 -0600

    (fix) build on linux

--- Sources/DataScanner/Endianness.swift
+++ Sources/DataScanner/Endianness.swift
@@ -5,10 +5,27 @@ public enum Endianness {
 	case big
 
 	public static let systemEndianness: Endianness = {
+		#if canImport(Darwin)
 		let system = CFByteOrderGetCurrent()
 		return Endianness(system) ?? .little
+		#else
+		let testValue: UInt32 = 0x04030201
+
+		let buffer = UnsafeMutableBufferPointer<UInt32>.allocate(capacity: 1)
+		defer { buffer.deallocate() }
+		buffer[0] = testValue
+
+		let rawBuffer = UnsafeMutableRawBufferPointer(buffer)
+
+		if rawBuffer[0] == 1 {
+			return .little
+		} else {
+			return .big
+		}
+		#endif
 	}()
 
+	#if canImport(Darwin)
 	init?(_ system: __CFByteOrder) {
 		switch system {
 		case CFByteOrderBigEndian:
@@ -23,4 +40,5 @@ public enum Endianness {
 		let systemType = __CFByteOrder(UInt32(byteOrder))
 		self.init(systemType)
 	}
+	#endif
 }
